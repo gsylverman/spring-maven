7b10a9d3fc5c7c5023f59eae88f1334650b6b354
gsylverman <gsylverman@gmail.com>
2020-10-13 22:51:16 +0300

    logback

--- src/main/java/some/maven/Hello.java
+++ src/main/java/some/maven/Hello.java
@@ -1,7 +1,12 @@
 package some.maven;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Hello {
+    private static final Logger logger = LoggerFactory.getLogger(Hello.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello maven");
+        logger.debug("aaaa");
     }
 }
